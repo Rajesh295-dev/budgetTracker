491ce942c752066c8561a189c6ba99badb1ead35
Rajesh295-dev <{rajeshgautam766@yahoo.com}+{Rajesh295-dev}@users.noreply.github.com>
2024-02-27 21:35:58 -0500

    added cors

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const compression = require("compression");
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+const corsOptions = {
+  origin: "https://new-budget-tracker.vercel.app", // Allow requests from this origin
+  methods: "GET,POST,PUT,DELETE", // Allow these HTTP methods
+  optionsSuccessStatus: 200, // Return 200 for preflight requests
+};
+
+app.use(cors(corsOptions));
 
 app.use(logger("dev"));
 
