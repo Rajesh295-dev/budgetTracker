01200884e624a12935ae8e0274396dce9d4e0c6b
Rajesh295-dev <{rajeshgautam766@yahoo.com}+{Rajesh295-dev}@users.noreply.github.com>
2024-02-27 22:14:24 -0500

    logging middleware added

--- server.js
+++ server.js
@@ -19,10 +19,17 @@ app.use(cors(corsOptions));
 
 app.use(logger("dev"));
 
+
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// Logging middleware for requests
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 
 
